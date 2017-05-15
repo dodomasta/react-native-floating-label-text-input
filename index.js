7f93bb2b7e7034739459c323f0fc543a4f9c4da2
dodomasta <jdaunar@gmail.com>
2017-05-15 18:26:15 +0100

    allow textinput to grow

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
-import React, { Component } from 'react';
+import React, { Component } from "react"
 import {
   StyleSheet,
   Text,
@@ -6,21 +6,22 @@ import {
   TextInput,
   Animated,
   Platform
-} from 'react-native'
+} from "react-native"
 
 class FloatingLabel extends Component {
   constructor(props) {
-    super(props);
+    super(props)
 
-    let initialPadding = 9;
-    let initialOpacity = 0;
+    let initialPadding = 9
+    let initialOpacity = 0
 
     if (this.props.visible) {
-      initialPadding = 5;
-      initialOpacity = 1;
+      initialPadding = 5
+      initialOpacity = 1
     }
 
     this.state = {
+      height: 45,
       paddingAnim: new Animated.Value(initialPadding),
       opacityAnim: new Animated.Value(initialOpacity)
     }
@@ -30,26 +31,34 @@ class FloatingLabel extends Component {
     Animated.timing(this.state.paddingAnim, {
       toValue: newProps.visible ? 5 : 9,
       duration: 230
-    }).start();
+    }).start()
 
     return Animated.timing(this.state.opacityAnim, {
       toValue: newProps.visible ? 1 : 0,
       duration: 230
-    }).start();
+    }).start()
   }
 
   render() {
     return (
-      <Animated.View style={[styles.floatingLabel, { paddingTop: this.state.paddingAnim, opacity: this.state.opacityAnim }]}>
+      <Animated.View
+        style={[
+          styles.floatingLabel,
+          {
+            paddingTop: this.state.paddingAnim,
+            opacity: this.state.opacityAnim
+          }
+        ]}
+      >
         {this.props.children}
       </Animated.View>
-    );
+    )
   }
 }
 
 class TextFieldHolder extends Component {
   constructor(props) {
-    super(props);
+    super(props)
     this.state = {
       marginAnim: new Animated.Value(this.props.withValue ? 10 : 0)
     }
@@ -59,7 +68,7 @@ class TextFieldHolder extends Component {
     return Animated.timing(this.state.marginAnim, {
       toValue: newProps.withValue ? 10 : 0,
       duration: 230
-    }).start();
+    }).start()
   }
 
   render() {
@@ -67,116 +76,142 @@ class TextFieldHolder extends Component {
       <Animated.View style={{ marginTop: this.state.marginAnim }}>
         {this.props.children}
       </Animated.View>
-    );
+    )
   }
 }
 
 class FloatLabelTextField extends Component {
   constructor(props) {
-    super(props);
+    super(props)
     this.state = {
       focused: false,
       text: this.props.value
-    };
+    }
   }
 
   componentWillReceiveProps(newProps) {
-    if (newProps.hasOwnProperty('value') && newProps.value !== this.state.text) {
+    if (
+      newProps.hasOwnProperty("value") &&
+      newProps.value !== this.state.text
+    ) {
       this.setState({ text: newProps.value })
     }
   }
 
   withBorder() {
     if (!this.props.noBorder) {
-      return styles.withBorder;
+      return styles.withBorder
     }
   }
 
   render() {
     return (
-      <View style={styles.container}>
+      <View
+        style={[
+          {
+            flex: 1,
+            height: Math.max(45, this.state.height + 16),
+            backgroundColor: "white",
+            justifyContent: "center"
+          }
+        ]}
+      >
         <View style={styles.viewContainer}>
           <View style={styles.paddingView} />
           <View style={[styles.fieldContainer, this.withBorder()]}>
             <FloatingLabel visible={this.state.text}>
-              <Text style={[styles.fieldLabel, this.labelStyle()]}>{this.placeholderValue()}</Text>
+              <Text style={[styles.fieldLabel, this.labelStyle()]}>
+                {this.placeholderValue()}
+              </Text>
             </FloatingLabel>
             <TextFieldHolder withValue={this.state.text}>
-              <TextInput {...this.props}
-                ref='input'
+              <TextInput
+                {...this.props}
+                multiline={true}
+                ref="input"
                 underlineColorAndroid="transparent"
-                style={[styles.valueText]}
+                // style={[styles.valueText]}
+                style={[
+                  {
+                    // height: (Platform.OS == 'ios' ? 20 : 60),
+                    fontSize: 16,
+                    color: "#111111",
+                    height: Math.max(45, this.state.height)
+                  }
+                ]}
                 defaultValue={this.props.defaultValue}
                 value={this.state.text}
                 maxLength={this.props.maxLength}
                 onFocus={() => this.setFocus()}
                 onBlur={() => this.unsetFocus()}
-                onChangeText={(value) => this.setText(value)}
-                />
+                onChange={value => this.setText(value)}
+              />
             </TextFieldHolder>
           </View>
         </View>
       </View>
-    );
+    )
   }
 
   inputRef() {
-    return this.refs.input;
+    return this.refs.input
   }
 
   focus() {
-    this.inputRef().focus();
+    this.inputRef().focus()
   }
 
   blur() {
-    this.inputRef().blur();
+    this.inputRef().blur()
   }
 
   isFocused() {
-    return this.inputRef().isFocused();
+    return this.inputRef().isFocused()
   }
 
   clear() {
-    this.inputRef().clear();
+    this.inputRef().clear()
   }
 
   setFocus() {
     this.setState({
       focused: true
-    });
+    })
     try {
-      return this.props.onFocus();
-    } catch (_error) { }
+      return this.props.onFocus()
+    } catch (_error) {}
   }
 
   unsetFocus() {
     this.setState({
       focused: false
-    });
+    })
     try {
-      return this.props.onBlur();
-    } catch (_error) { }
+      return this.props.onBlur()
+    } catch (_error) {}
   }
 
   labelStyle() {
     if (this.state.focused) {
-      return styles.focused;
+      return styles.focused
     }
   }
 
   placeholderValue() {
     if (this.state.text) {
-      return this.props.placeholder;
+      return this.props.placeholder
     }
   }
 
   setText(value) {
     this.setState({
-      text: value
-    });
+      // text: value
+      text: value.nativeEvent.text,
+      height: value.nativeEvent.contentSize.height
+    })
     try {
-      return this.props.onChangeTextValue(value);
-    } catch (_error) { }
+      return this.props.onChangeTextValue(value)
+    } catch (_error) {}
   }
 }
 
@@ -184,43 +219,43 @@ const styles = StyleSheet.create({
   container: {
     flex: 1,
     height: 45,
-    backgroundColor: 'white',
-    justifyContent: 'center'
+    backgroundColor: "white",
+    justifyContent: "center"
   },
   viewContainer: {
     flex: 1,
-    flexDirection: 'row'
+    flexDirection: "row"
   },
   paddingView: {
     width: 15
   },
   floatingLabel: {
-    position: 'absolute',
+    position: "absolute",
     top: 0,
     left: 0
   },
   fieldLabel: {
     height: 15,
     fontSize: 10,
-    color: '#B1B1B1'
+    color: "#B1B1B1"
   },
   fieldContainer: {
     flex: 1,
-    justifyContent: 'center',
-    position: 'relative'
+    justifyContent: "center",
+    position: "relative"
   },
   withBorder: {
     borderBottomWidth: 1 / 2,
-    borderColor: '#C8C7CC',
+    borderColor: "#C8C7CC"
   },
   valueText: {
-    height: (Platform.OS == 'ios' ? 20 : 60),
+    height: Platform.OS == "ios" ? 20 : 60,
     fontSize: 16,
-    color: '#111111'
+    color: "#111111"
   },
   focused: {
     color: "#1482fe"
   }
-});
+})
 
-export default FloatLabelTextField;
+export default FloatLabelTextField
